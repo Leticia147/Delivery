1af077647dc92c4112140b6d39203e8f4a54ef2a
leticia <leticia@debian-BULLSEYE-live-builder-AMD64>
2022-05-11 22:41:42 -0300

    ultima versao

--- Codigos/cadastraDrone.py
+++ Codigos/cadastraDrone.py
@@ -9,25 +9,29 @@ w3.eth.default_account = w3.eth.accounts[1]
 
 
 # get abi
-abi = abi = [{'inputs': [], 'stateMutability': 'payable', 'type': 'constructor'}, {'inputs': [], 'name': 'cadastrarDrone', 'outputs': [], 'stateMutability': 'nonpayable', 'type': 'function'}, {'inputs': [], 'name': 'contractOwner', 'outputs': [{'internalType': 'address payable', 'name': '', 'type': 'address'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [], 'name': 'deposit', 'outputs': [], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'name': 'dronesCadastrados', 'outputs': [{'internalType': 'address payable', 'name': 'droneOwner', 'type': 'address'}, {'internalType': 'string', 'name': 'latitude_deg', 'type': 'string'}, {'internalType': 'string', 'name': 'longitude_deg', 'type': 'string'}, {'internalType': 'uint256', 'name': 'flying', 'type': 'uint256'}, {'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [], 'name': 'getBalance', 'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}, {'internalType': 'address', 'name': '', 'type': 'address'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'getDestino', 'outputs': [{'internalType': 'string', 'name': '', 'type': 'string'}, {'internalType': 'string', 'name': '', 'type': 'string'}, {'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'inTheDestiny', 'outputs': [{'internalType': 'address', 'name': '', 'type': 'address'}, {'internalType': 'address payable', 'name': '', 'type': 'address'}], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'setDestinoOne', 'outputs': [{'internalType': 'bool', 'name': '', 'type': 'bool'}], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'amount', 'type': 'uint256'}, {'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'setDestinoThree', 'outputs': [], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'amount', 'type': 'uint256'}, {'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'setDestinoTwo', 'outputs': [], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [], 'name': 'totalDronesCadastrados', 'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}]
-contract_id = Web3.toChecksumAddress('0xf29eac3c3e6c40cade21511a03506f7d515ed9dd')
+abi = [{'inputs': [], 'stateMutability': 'payable', 'type': 'constructor'}, {'inputs': [], 'name': 'cadastrarDrone', 'outputs': [], 'stateMutability': 'nonpayable', 'type': 'function'}, {'inputs': [], 'name': 'contractOwner', 'outputs': [{'internalType': 'address payable', 'name': '', 'type': 'address'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [], 'name': 'deposit', 'outputs': [], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'name': 'dronesCadastrados', 'outputs': [{'internalType': 'address payable', 'name': 'droneOwner', 'type': 'address'}, {'internalType': 'string', 'name': 'latitude_deg', 'type': 'string'}, {'internalType': 'string', 'name': 'longitude_deg', 'type': 'string'}, {'internalType': 'uint256', 'name': 'flying', 'type': 'uint256'}, {'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [], 'name': 'getBalance', 'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'getStatusDrone', 'outputs': [{'internalType': 'string', 'name': '', 'type': 'string'}, {'internalType': 'string', 'name': '', 'type': 'string'}, {'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [], 'name': 'recieve', 'outputs': [], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'setDestinoOne', 'outputs': [{'internalType': 'bool', 'name': '', 'type': 'bool'}], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'setDestinoThree', 'outputs': [{'internalType': 'bool', 'name': '', 'type': 'bool'}], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'setDestinoTwo', 'outputs': [{'internalType': 'bool', 'name': '', 'type': 'bool'}], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [], 'name': 'totalDronesCadastrados', 'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'withdraw', 'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'stateMutability': 'payable', 'type': 'function'}]
+contract_id = Web3.toChecksumAddress('0x747ff7bd12b47fead567f775f3a9de0e48ff3fe4')
 deposita = w3.eth.contract(address = contract_id,abi = abi)
 
-receipt =  w3.eth.wait_for_transaction_receipt( deposita.functions.cadastrarDrone().transact())
+#receipt =  w3.eth.wait_for_transaction_receipt( deposita.functions.cadastrarDrone().transact())
 print( deposita.functions.totalDronesCadastrados().call())
 
-
+idDrone = 1
 #transaction = w3.eth.get_transaction(w3.toHex(w3.eth.send_transaction(tx)))  #transferindo valor
+#print(transaction)
+
+
+
+#transaction = w3.eth.get_transaction('0xe8ca4a6bce23e1a1ddf23e75509601ecb35367966a5692056cee66d49aaaf6b9') 
 #print(transaction)
 
 #print( deposita.functions.getBalance().call())
 
-#print( deposita.functions.getBalance().call())
+#print(deposita.functions.setDestinoOne(idDrone).transact({'from': w3.eth.default_account,'value': w3.toWei(10,'ether')}))
 
 
-#print(deposita.functions.setDestinoOne(1).transact({'from': w3.eth.accounts[2],'value': w3.toWei(2,'ether')}))
+print( deposita.functions.withdraw(1).transact())
 
-print( deposita.functions.inTheDestiny(1).call())
 
 print(w3.eth.get_balance(w3.eth.accounts[0])/1000000000000000000)
 print(w3.eth.get_balance(w3.eth.accounts[1])/1000000000000000000)
--- Codigos/controlDrone.py
+++ Codigos/controlDrone.py
@@ -22,12 +22,12 @@ from solcx import compile_source
 
 def atDestino(atual_latitude_deg,dest_latitude_deg, atual_longitude_deg,  dest_longitude_deg):
     n = 4
-    m = n+1
+    m = n
     atual_latitude_deg_truncated = int(float(atual_latitude_deg) * 10**n)/10**n
     atual_longitude_deg_truncated = int(float(atual_longitude_deg) * 10**m)/10**m
     dest_latitude_deg_truncated = int(float(dest_latitude_deg) * 10**n)/10**n
     dest_longitude_deg_truncated = int(float(dest_longitude_deg) * 10**m)/10**m
-
+    print("atDestino?")
     if(atual_latitude_deg_truncated == dest_latitude_deg_truncated and atual_longitude_deg_truncated == dest_longitude_deg_truncated):
         print("são iguais")
         return True
@@ -41,25 +41,30 @@ async def run():
     w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 
     # set pre-funded account as sender
-    w3.eth.default_account = w3.eth.accounts[0]
+    w3.eth.default_account = w3.eth.accounts[1]
 
-    abi = [{'inputs': [], 'stateMutability': 'nonpayable', 'type': 'constructor'}, {'inputs': [], 'name': 'cadastrarDrone', 'outputs': [], 'stateMutability': 'nonpayable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'name': 'dronesCadastrados', 'outputs': [{'internalType': 'address payable', 'name': 'droneOwner', 'type': 'address'}, {'internalType': 'string', 'name': 'latitude_deg', 'type': 'string'}, {'internalType': 'string', 'name': 'longitude_deg', 'type': 'string'}, {'internalType': 'uint256', 'name': 'flying', 'type': 'uint256'}, {'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'getDestino', 'outputs': [{'internalType': 'string', 'name': '', 'type': 'string'}, {'internalType': 'string', 'name': '', 'type': 'string'}, {'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'inTheDestiny', 'outputs': [{'internalType': 'address', 'name': '', 'type': 'address'}, {'internalType': 'address payable', 'name': '', 'type': 'address'}], 'stateMutability': 'nonpayable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'amount', 'type': 'uint256'}, {'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'setDestinoOne', 'outputs': [{'internalType': 'bool', 'name': '', 'type': 'bool'}], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'amount', 'type': 'uint256'}, {'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'setDestinoThree', 'outputs': [], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'amount', 'type': 'uint256'}, {'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'setDestinoTwo', 'outputs': [], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [], 'name': 'totalDronesCadastrados', 'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}]
-    contract_id = Web3.toChecksumAddress('0x4d0f175cb3c9ebbae371dcdb536c8aafada28eba')
-    delivery = w3.eth.contract(address = contract_id,abi = abi)
+    # get abi
+    abi = abi = [{'inputs': [], 'stateMutability': 'payable', 'type': 'constructor'}, {'inputs': [], 'name': 'cadastrarDrone', 'outputs': [], 'stateMutability': 'nonpayable', 'type': 'function'}, {'inputs': [], 'name': 'contractOwner', 'outputs': [{'internalType': 'address payable', 'name': '', 'type': 'address'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [], 'name': 'deposit', 'outputs': [], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'name': 'dronesCadastrados', 'outputs': [{'internalType': 'address payable', 'name': 'droneOwner', 'type': 'address'}, {'internalType': 'string', 'name': 'latitude_deg', 'type': 'string'}, {'internalType': 'string', 'name': 'longitude_deg', 'type': 'string'}, {'internalType': 'uint256', 'name': 'flying', 'type': 'uint256'}, {'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [], 'name': 'getBalance', 'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}, {'internalType': 'address', 'name': '', 'type': 'address'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'getStatusDrone', 'outputs': [{'internalType': 'string', 'name': '', 'type': 'string'}, {'internalType': 'string', 'name': '', 'type': 'string'}, {'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'inTheDestiny', 'outputs': [{'internalType': 'address', 'name': '', 'type': 'address'}, {'internalType': 'address payable', 'name': '', 'type': 'address'}], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'setDestinoOne', 'outputs': [{'internalType': 'bool', 'name': '', 'type': 'bool'}], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'setDestinoThree', 'outputs': [{'internalType': 'bool', 'name': '', 'type': 'bool'}], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [{'internalType': 'uint256', 'name': 'idDrone', 'type': 'uint256'}], 'name': 'setDestinoTwo', 'outputs': [{'internalType': 'bool', 'name': '', 'type': 'bool'}], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [], 'name': 'totalDronesCadastrados', 'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}]
+    contract_id = Web3.toChecksumAddress('0xca0ce4b7eafc7a6a773be5015c5c6d0eb52837c7')
+    deliveryDrone = w3.eth.contract(address = contract_id,abi = abi)
+    w3.eth.wait_for_transaction_receipt( deliveryDrone.functions.cadastrarDrone().transact())
 
-    print(delivery.functions.getDestino().call())
+    idDrone = 1
+     
+    print(deliveryDrone.functions.getStatusDrone(idDrone).call())
 
     while True:
          
-        while delivery.functions.getDestino().call()[2] != 1: 
+        while deliveryDrone.functions.getStatusDrone(idDrone).call()[2] != 1: 
             print("Aguardando voo")
+            print(w3.eth.get_balance(w3.eth.default_account)/1000000000000000000)
             time.sleep(5)    
     
         #ler dados de uma trsação especifica 
         #transaction = w3.eth.get_transaction('0x8d2ed830adf2285fb6f80c542852fdc329083085658fe019c8c49d61c0fb09cd') 
         #print(delivery.decode_function_input(transaction.input))
 
-        if delivery.functions.getDestino().call()[2] == 1:
+        if deliveryDrone.functions.getStatusDrone(idDrone).call()[2] == 1:
             await drone.connect(system_address="udp://:14540")
 
             print("Waiting for drone to connect...")
@@ -85,27 +90,31 @@ async def run():
 
             await asyncio.sleep(1)
 
-            print("-- Going to position: ", delivery.functions.getDestino().call())
-            await drone.action.goto_location(float(delivery.functions.getDestino().call()[0]),float(delivery.functions.getDestino().call()[1]), 100 , 0)
+            print("-- Going to position: ", deliveryDrone.functions.getStatusDrone(idDrone).call())
+            await drone.action.goto_location(float(deliveryDrone.functions.getStatusDrone(idDrone).call()[0]),float(deliveryDrone.functions.getStatusDrone(idDrone).call()[1]), 100 , 0)
 
             while True:
                 print("Staying connected, press Ctrl-C to exit")
                 async for position in drone.telemetry.position():
-                    print(position," to " , delivery.functions.getDestino().call())
+                    print(position," to " , deliveryDrone.functions.getStatusDrone(idDrone).call())
                     await asyncio.sleep(5)
-                    print( position.latitude_deg, delivery.functions.getDestino().call()[0],position.longitude_deg, delivery.functions.getDestino().call()[1])
-                    if(atDestino( position.latitude_deg, delivery.functions.getDestino().call()[0],position.longitude_deg, delivery.functions.getDestino().call()[1]) == True): 
+                    print( position.latitude_deg, deliveryDrone.functions.getStatusDrone(idDrone).call()[0],position.longitude_deg, deliveryDrone.functions.getStatusDrone(idDrone).call()[1])
+                    if(atDestino( position.latitude_deg, deliveryDrone.functions.getStatusDrone(idDrone).call()[0],position.longitude_deg, deliveryDrone.functions.getStatusDrone(idDrone).call()[1]) == True): 
                         print("-- Chegou ao destino!")
                         await drone.action.land()
                         print("Pousando at" ,position)
-                        if(position.relative_altitude_m < 2):
-                            print("Pousando at" ,position)
-                            await asyncio.sleep(3)
-                            delivery.functions.inTheDestiny().transact()
-                if(delivery.functions.getDestino().call()[2] == 0): 
-                    print("Pousado!")
-                    break
+                        async for in_air in drone.telemetry.in_air():
+                            if in_air:
+                                print("in air! Pousando")
+                            else:
+                                print("on ground! Pousado")
+                                deliveryDrone.functions.inTheDestiny(idDrone).transact()
+                                break
+                        break  
+                break
             print("Delivery Sucessfull complete!")
+            print(w3.eth.get_balance(w3.eth.default_account)/1000000000000000000)
+
 
 async def print_status_text(drone):
     try:
